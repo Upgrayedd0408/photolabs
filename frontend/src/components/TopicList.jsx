d2a8f506a8212e374a9d3a0707c365c2d3bb0c32
Upgrayedd0408 <michaellowry89@gmail.com>
2024-04-03 12:47:56 -0700

    remove default props and pass in Mock Data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,34 +1,19 @@
 import React from "react";
 import TopicListItem from "./TopicListItem";
+import topics from "mocks/topics";
+
 
 import "../styles/TopicList.scss";
 
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
 
 const TopicList = () => {
 
-  const topics = sampleDataForTopicList.map((topic) => <TopicListItem key={topic.id} topic={topic} /* fav={fav} favPhoto={favPhoto} */ />);
+  const listOfTopics = topics.map((topic) => <TopicListItem key={topic.id} topic={topic} /* fav={fav} favPhoto={favPhoto} */ />);
 
   return (
     <div className="top-nav-bar__topic-list">
-      {topics}
+      {listOfTopics}
     </div>
   );
 };
